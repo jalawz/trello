c85c3bb2afd690d9b1e98f284ba95afe7b8fd46a
paulomenezes-sciensa <paulo.roberto@soaexpert.com.br>
2017-05-10 16:43:21 -0300

    Delete action in another story model hook

--- app/routes/stories/story.js
+++ app/routes/stories/story.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('story', params.id, {include: 'tasks'});
+  },
+
+  deleteTask(task) {
+    let confirmation = confirm('Are you sure?');
+
+    if (confirmation) {
+      task.destroyRecord().then(() => this.transitionTo('stories'));
+    }
   }
 });
