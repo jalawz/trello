c2de624210c8da8e6b3e467a4f7f81376533aa8c
paulomenezes-sciensa <paulo.roberto@soaexpert.com.br>
2017-05-10 15:06:32 -0300

    Creating edit controller

--- app/routes/stories/story.js
+++ app/routes/stories/story.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('story', params.id);
+    return this.store.findRecord('story', params.id, {include: 'tasks'});
   }
 });
