1caa9325bcd055a795c024e7bbaab22aa8253ba5
paulomenezes-sciensa <paulo.roberto@soaexpert.com.br>
2017-05-10 13:37:10 -0300

    Willtransition model hook on edit task

--- app/routes/stories/edit.js
+++ app/routes/stories/edit.js
@@ -14,10 +14,8 @@ export default Ember.Route.extend({
 
     willTransition(transition) {
       let model = this.controller.get('model');
-
       if(model.get('hasDirtyAttributes')) {
         let confirmation = confirm("Your changes haven't saved yet. Would you like to leave this form?");
-
         if (confirmation) {
           model.rollbackAttributes();
         } else {
--- app/routes/tasks/edit.js
+++ app/routes/tasks/edit.js
@@ -8,6 +8,18 @@ export default Ember.Route.extend({
   actions: {
     saveTask(task) {
       task.save().then(() => this.transitionTo('tasks'));
+    },
+
+    willTransition(transition) {
+      let model = this.controller.get('model');
+      if(model.get('hasDirtyAttributes')) {
+        let confirmation = confirm("Your changes haven't saved yet. Would you like to leave this form?");
+        if (confirmation) {
+          model.rollbackAttributes();
+        } else {
+          transition.abort();
+        }
+      }
     }
   }
 });
