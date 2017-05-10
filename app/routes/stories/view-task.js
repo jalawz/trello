2535f256bda0e21648dc2672c223840bf0b3cb4e
paulomenezes-sciensa <paulo.roberto@soaexpert.com.br>
2017-05-10 16:22:14 -0300

    Delete task link

--- app/routes/stories/view-task.js
+++ app/routes/stories/view-task.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('task', params.task_id);
+  },
+
+  actions: {
+    deleteTask(task) {
+      
+    }
   }
 });
