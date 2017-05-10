6b7fc5407e433b828a40542f57f96969f30fcbf4
paulomenezes-sciensa <paulo.roberto@soaexpert.com.br>
2017-05-10 13:29:57 -0300

    Save action in edit route

--- app/routes/tasks/edit.js
+++ app/routes/tasks/edit.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('task', params.id);
+  },
+
+  actions: {
+    saveTask(task) {
+      task.save().then(() => this.transitionTo('tasks'));
+    }
   }
 });
