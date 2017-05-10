35d95bd8df11c396c0586d9b947d54435e7e8f7a
paulomenezes-sciensa <paulo.roberto@soaexpert.com.br>
2017-05-10 11:36:54 -0300

    Add stories feature added

--- app/routes/stories/new.js
+++ app/routes/stories/new.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('story');
+  },
+
+  actions: {
+    saveStory(story) {
+      story.save().then(() => this.transitionTo('stories'));
+    }
+  }
 });
