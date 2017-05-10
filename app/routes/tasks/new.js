cf7cc8a0716a61b8997c1d2967e5a0173e04f3ac
paulomenezes-sciensa <paulo.roberto@soaexpert.com.br>
2017-05-10 14:29:33 -0300

    Including stories in task form

--- app/routes/tasks/new.js
+++ app/routes/tasks/new.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.createRecord('task');
+    return Ember.RSVP.hash({
+      task: this.store.createRecord('task'),
+      stories: this.store.findAll('story')
+    });
   },
 
   actions: {
