4cd7621f8d347ed8f74cdaa0f74d3781fc6a5d29
paulomenezes-sciensa <paulo.roberto@soaexpert.com.br>
2017-05-10 13:13:26 -0300

    Savetask action added

--- app/routes/tasks/new.js
+++ app/routes/tasks/new.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.createRecord('task');
+  },
+
+  actions: {
+    saveTask(task) {
+      task.save().then(() => this.transitionTo('tasks'));
+    }
   }
 });
