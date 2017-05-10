4184ec5d76d095d370723a4620be7cf1c82fb5a8
paulomenezes-sciensa <paulo.roberto@soaexpert.com.br>
2017-05-10 17:13:51 -0300

    Form validation

--- app/models/story.js
+++ app/models/story.js
@@ -3,5 +3,10 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   dueDate: DS.attr('date'),
-  tasks: DS.hasMany('task')
+  tasks: DS.hasMany('task'),
+
+  isValidTitle: Ember.computed.notEmpty('title'),
+  isValidDate: Ember.computed.notEmpty('dueDate'),
+
+  isValid: Ember.computed.and('isValidTitle', 'isValidDate')
 });
